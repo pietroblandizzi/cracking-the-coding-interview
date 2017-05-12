26147153526e66d5df125f7bed825fe2ea3c411e
pietroblandizzi <pietrosqualo@hotmail.it>
2017-05-12 16:37:02 +0200

    fix

--- 3/3_1.cpp
+++ 3/3_1.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <exception>
 #include <string>
-
+/*************FIXED************************/
+/*
 class MultiStack
 {
     const int numOfStack = 3;
@@ -82,13 +83,197 @@ void MultiStack::printAll()
     }
     std::cout<<std::endl;
 }
+*/
+//forward declaration
+class StackInfo;
 
+class MultiStack
+{
+    friend class StackInfo;
+    int numOfStack;
+    int* values;
+    int v_size;
+    StackInfo* info;
+
+public:
+    MultiStack (int numOfStacks, int defaultSize);
+    ~MultiStack ();
+    bool isEmpty (int n);
+    int peek (int n);  // n stack number
+    void push (int n, int data);
+    int pop (int n);
+    void printAll();
+    void expand(int n);
+    int numOfElements();
+    int adjustIndex(int n);
+    bool areAllStacksFull ();
+    int previousIndex (int index);
+    int nextIndex(int index);
+    void shift (int n);
+
+};
+
+class StackInfo {
+      public:
+        MultiStack* parent;
+        int stack_size;
+        int capacity;
+        int start;
+        int v_size;
+
+        StackInfo ();
+        void setParam (MultiStack* ms,int s,int c,int v) {
+            parent = ms;
+            start = s;
+            capacity = c;
+            stack_size = 0;
+            v_size = v;
+        }
+
+       bool isWhithinStackCapacity (int index) {
+            if (index <0 || index> v_size)
+                return false;
+
+           int cInd = index < start ? index+v_size : index;
+           int endStack = start+capacity;
+           return start <= cInd && cInd < endStack;
+       }
+
+       int lastCapacityIndex () {
+        return parent->adjustIndex (start+capacity-1);
+       }
+
+        int lastElementIndex () {
+           return parent->adjustIndex (start+stack_size-1);
+        }
+
+        bool isFull () {return stack_size == capacity;}
+        bool isEmpty () {return stack_size == 0;}
+
+    };
+
+
+StackInfo::StackInfo() {}
+
+MultiStack::MultiStack (int numOfStacks, int defaultSize)
+{
+    numOfStack = numOfStacks;
+    v_size = defaultSize*numOfStack;
+    info = new StackInfo[numOfStacks];
+    values = new int[v_size];
+    for (int i = 0; i<numOfStack; i++) {
+        info[i].setParam(this, defaultSize*i,defaultSize,v_size);
+    }
+    for (int i = 0; i<v_size; i++) {
+        values[i] = 0;
+    }
+}
+
+MultiStack::~MultiStack ()
+{
+    delete[] values;
+    delete[] info;
+}
+
+bool MultiStack::isEmpty(int n)
+{
+    return info[n].stack_size == 0;
+}
+
+int MultiStack::peek(int n)
+{
+    if (info[n].isEmpty())
+        throw  std::string("Empty stack");
+    return values [info[n].lastElementIndex()];
+}
+
+int MultiStack::pop (int n) {
+    if (info[n].isEmpty())
+        throw  std::string("Empty stack");
+
+    int value = values [info[n].lastElementIndex()];
+    info[n].stack_size--;
+    return value;
+}
+
+void MultiStack::push (int n,int data)
+{
+   // if all stacks are full we cannot shift values
+   if (areAllStacksFull ())
+    throw std::string("stacks is full");
+
+    //if this stack is full expand it
+    if (info[n].isFull()) {
+        expand (n);
+    }
+    info[n].stack_size++;
+    values[info[n].lastElementIndex()] = data;
+}
+
+void MultiStack::printAll()
+{
+    for (int i = 0; i< v_size;i++) {
+        std::cout << values[i] << " ";
+    }
+    std::cout<<std::endl;
+}
+
+
+void MultiStack::shift(int n) {
+    if (info[n].stack_size>=info[n].capacity) {
+        int nextStack = (n+1)%numOfStack;
+        shift(nextStack);
+        info[n].capacity++;
+    }
+    int index = info[n].lastCapacityIndex();
+    while (info[n].isWhithinStackCapacity(index)) {
+        values [index] = values [previousIndex(index)];
+        index = previousIndex (index);
+    }
+    values[info[n].start] = 0;
+    info[n].start = nextIndex (info[n].start);
+    info[n].capacity--;
+}
+
+void MultiStack::expand (int n)
+{
+    shift ((n+1)%numOfStack);
+    info[n].capacity++;
+}
+
+int MultiStack::numOfElements ()
+{
+    int sizeTot = 0;
+    for (int i = 0 ; i<numOfStack; i++) {
+        sizeTot += info[i].stack_size;
+    }
+    return sizeTot;
+}
+
+bool MultiStack::areAllStacksFull ()
+{
+    return numOfElements() == v_size;
+}
+
+int MultiStack::adjustIndex (int index)
+{
+    return ((index%v_size)+ v_size) %v_size;
+}
+
+int MultiStack::nextIndex (int index)
+{
+    return adjustIndex(index + 1);
+}
+
+int MultiStack::previousIndex (int index)
+{
+    return adjustIndex(index - 1);
+}
 
 int main()
 {
-    try {
     //3 Stack array of 10 values;
-    MultiStack ms(3);
+    MultiStack ms(3,5);
     if (ms.isEmpty(0)) std::cout << "stack 1 empty" << std::endl;
     if (ms.isEmpty(1)) std::cout << "stack 2 empty" << std::endl;
     if (ms.isEmpty(2)) std::cout << "stack 3 empty" << std::endl;
@@ -103,12 +288,10 @@ int main()
     ms.printAll();
 
     std::cout << ms.peek(2) << std::endl;
-    std::cout << ms.pop(2) << std::endl;
+    ms.pop(2);
     ms.push(2,7);
-    ms.push(2,7);
-    ms.push(0,7);
     ms.printAll();
 
-    }catch (std::string e) {std::cout << e;}
+
     return 0;
 }
