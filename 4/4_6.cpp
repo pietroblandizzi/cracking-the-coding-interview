c04815b47702a538e5bc255103e857e174612c1d
pietroblandizzi <pietrosqualo@hotmail.it>
2017-05-24 14:16:20 +0200

    Add 4.5 4.6

--- 4/4_6.cpp
+++ 4/4_6.cpp
@@ -34,11 +34,10 @@ public:
     int data;
     NodeBT* left;
     NodeBT* right;
+    NodeBT* parent;
 };
 
-bool isSmaller (NodeBT*, int );
-
-NodeBT* utilityCreate (vector<int>& v, int start,int end)
+NodeBT* utilityCreate (vector<int>& v, int start,int end, NodeBT* parent)
 {
    if (end < start)
         return nullptr;
@@ -46,86 +45,59 @@ NodeBT* utilityCreate (vector<int>& v, int start,int end)
     NodeBT* n = new NodeBT;
     // divide the array in two
     int middle = (start+end)/2;
-
+    n->parent = parent;
     n->data = v[middle];
-    n->left = utilityCreate (v,start, middle-1);
-    n->right = utilityCreate (v,middle+1, end);
+    n->left = utilityCreate (v,start, middle-1,n);
+    n->right = utilityCreate (v,middle+1, end,n);
 
     return n;
 
 }
-/** My solution is maybe a bit complicate
-It check for each subtree is a BST
-checking if left < n < data and then
-compute the max value in the left three
-and check if it is bigger than it
-and the same with the min value of the right
-and check if it is smaller than it
-**
-Some int suggest to flip the logic
-we can check if curr is always big then left
-and always smaller then right
-**/
-
-/***** first solution****/
-int biggestLeft (NodeBT* n)
-{
-    if (n == nullptr)
-        return INT_MIN;
-
-    return std::max (n->data,biggestLeft(n->left));
-}
-
-int smallestRight (NodeBT* n)
-{
-    if (n == nullptr)
-        return INT_MAX;
-
-    return std::min (n->data, smallestRight(n->right));
-}
 
 NodeBT* buildMinHeightBST (vector<int>& v)
 {
-    return utilityCreate(v,0,v.size()-1);
+    return utilityCreate(v,0,v.size()-1,nullptr);
+}
+// my idea
+NodeBT* mostLeft (NodeBT* n)
+{
+    if(n == nullptr)
+        return nullptr;
+    while (n->left!= nullptr)
+        n = n->left;
+
+    return n;
 }
 
+NodeBT* successor (NodeBT* s)
+{
+    if (s == nullptr)
+        return nullptr;
 
-// idea from the solution we do an in-order traversal and check
- bool checkBST (NodeBT* n, int& prev)
- {
-     if (n == nullptr)
-        return true;
-     //go left
-     if (!checkBST(n->left, prev))
-        return false;
-     //check the current
-     if (prev != INT_MIN && n->data <= prev)
-        return false;
+    if (s->right == nullptr) {
+        NodeBT* p = s->parent;
+        while (p!=nullptr && p->data < s->data)
+            p = p->parent;
+        return p;
+    }
+    return mostLeft(s->right);
+}
 
-     prev = n->data;
+NodeBT* successorV2 (NodeBT* s)
+{
+    if (s == nullptr)
+        return nullptr;
 
-     //go right
-     if (!checkBST(n->right,prev))
-        return false;
-
-     return true;
- }
-
- //with min and max
- bool checkBSTV2 (NodeBT* n, int& r_min, int& r_max)
- {
-    if (n == nullptr)
-        return true;
-
-    if (n->data <= r_min || n->data > r_max )
-        return false;
-
-    bool left = checkBSTV2 (n->left, r_min, n->data);
-    bool right = checkBSTV2 (n->right,n->data,r_max);
-
-    return left && right;
-
- }
+    if (s->right == nullptr) {
+        NodeBT* n = s;
+        NodeBT* p = s->parent;
+        while (p!=nullptr && p->left != n)
+            n = p;
+            p = p->parent;
+        return p;
+    }
+    return mostLeft(s->right);
+}
 
 //preorder
 void printBSTPreOrd (NodeBT* root)
@@ -157,27 +129,17 @@ int main()
 
     NodeBT* root =buildMinHeightBST (v);
 
-
-     int d = INT_MIN;
-    if (checkBST(root,d))
-        std::cout << "yes" << std::endl;
-
-    else
-        std::cout << "no" << std::endl;
-
-     int r_min = INT_MIN;
-     int r_max = INT_MAX;
-
-    if (checkBSTV2(root,r_min,r_max))
-        std::cout << "yes" << std::endl;
-
-    else
-        std::cout << "no" << std::endl;
-
+    NodeBT* source = root->left->right; // 3
+    std::cout << "source " << source->data << std::endl;
+    std::cout << "parent " << source->parent->data << std::endl;
+    NodeBT* succ = successor(source);
+    if (succ != nullptr)
+        std::cout << "Successor " <<  succ->data <<std::endl;
+    succ = successorV2(source);
+    if (succ != nullptr)
+        std::cout << "Successor " <<  succ->data <<std::endl;
 
     printBSTPreOrd(root);
-
-    std::cout<< "root " << root->right->data ;
 
     return 0;
 }
